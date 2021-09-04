7451e2d5570fe05ddf721f698cfd72bce20910e9
M-i-a-S <mia.stajic@gmail.com>
2021-09-04 14:41:12 +0200

    Added Bootstrap

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import Circle1 from "./Circle1";
 import Circle2 from "./Circle2";
 import Form from "./Form";
 import Coder from "./Coder";
+import 'bootstrap/dist/css/bootstrap.css';
 
 import reportWebVitals from './reportWebVitals';
 
