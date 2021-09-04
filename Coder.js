7004dfc109a275a9ee1eb88ed15e4f5832f13cd8
M-i-a-S <mia.stajic@gmail.com>
2021-09-04 13:56:42 +0200

    Added the coder and the GitHub link description

---
+++ Coder.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Coder() {
+  return (
+    <div className="Coder">
+      <a href="https://github.com/M-i-a-S/Weather-app" target="_blank">
+        Open-source code
+      </a>
+      by Mia Stajić
+    </div>
+  );
+}
