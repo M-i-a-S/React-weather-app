9619d93937e5182280f4c99331217f47c7698843
M-i-a-S <mia.stajic@gmail.com>
2021-09-04 13:49:19 +0200

    Set up the aplication

--- src/index.js
+++ src/index.js
@@ -1,8 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import ImageSun from "./ImageSun";
+import Temp from "./Temp";
+import Description from "./Description";
+import Circle1 from "./Circle1";
+import Circle2 from "./Circle2";
+import Form from "./Form";
+import Coder from "./Coder";
+
 import reportWebVitals from './reportWebVitals';
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="Container">
+      <div className="App">
+        <h1>
+          <ImageSun />
+          <Temp />
+          <Description />
+        </h1>
+
+        <Circle1 />
+
+        <Circle2 />
+
+        <Form />
+
+        <hr className="line" />
+        <div className="days" id="forecast"></div>
+
+        <Coder />
+      </div>
+    </div>
+  );
+}
+
 
 ReactDOM.render(
   <React.StrictMode>
