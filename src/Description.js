5dbffa0497596254e52c4cba5e4499fc239fd01a
M-i-a-S <mia.stajic@gmail.com>
2021-09-04 13:53:00 +0200

    Added description inside the heading

---
+++ src/Description.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Description.css";
+
+export default function Description() {
+  return (
+    <div>
+      <em className="Description">Few clouds</em>
+    </div>
+  );
+}
