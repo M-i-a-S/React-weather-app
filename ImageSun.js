c412d4bf68dd38307c8a254f48dc63ec65d4b175
M-i-a-S <mia.stajic@gmail.com>
2021-09-04 13:54:56 +0200

    Added the image of the sun

---
+++ ImageSun.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./ImageSun.css";
+
+export default function ImageSun() {
+  return (
+    <img
+      src="http://openweathermap.org/img/wn/10d@2x.png"
+      alt="sun.gif"
+      className="Sun"
+      id="icon"
+    />
+  );
+}
