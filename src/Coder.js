b9d0afb3fcaea028111573dbf1316e2407f5ec12
M-i-a-S <mia.stajic@gmail.com>
2021-09-06 17:06:30 +0200

    Changed the url address

--- src/Coder.js
+++ src/Coder.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Coder() {
   return (
     <div className="Coder">
-      <a href="https://github.com/M-i-a-S/Weather-app" target="_blank" rel="noreferrer">
+      <a href="https://github.com/M-i-a-S/react-weather-app" target="_blank" rel="noreferrer">
         Open-source code
       </a>
       by Mia Stajić
